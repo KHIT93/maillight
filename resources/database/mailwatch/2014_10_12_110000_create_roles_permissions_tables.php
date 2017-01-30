103d536f817d0b9a5ab1086705662da1fd394c0a
Kenneth Hansen <kenneth@khansen-it.dk>
2017-01-30 19:54:40 +0100

    Fix naming issue of roles and permissions class

--- resources/database/mailwatch/2014_10_12_110000_create_roles_permissions_tables.php
+++ resources/database/mailwatch/2014_10_12_110000_create_roles_permissions_tables.php
@@ -2,7 +2,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateRolesPermissionsTable extends Migration
+class CreateRolesPermissionsTables extends Migration
 {
     /**
      * Run the migrations.
