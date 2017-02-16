447d378024e23441f8764026f5f3ca078f3d28b3
Kenneth Hansen <kenneth@vkdata.dk>
2017-02-16 15:10:47 +0100

    Update settings model to allow for setting values as well as get from db or fall back to config()

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -11,8 +11,35 @@ class Setting extends Model
     protected $guarded = ['uuid'];
     public $incrementing = false;
 
-    public static function get($key)
+    public static function get($key, $default = null)
     {
-    	return static::findByName($key);
+        $record = static::getFromDB($key);
+    	return (is_null($record)) ? config($key, $default) : $record->value;
+    }
+
+    protected static function getFromDB($key)
+    {
+        $record = static::findByName($key);
+    	return (is_object($record)) ? $record: null;
+    }
+
+    public static function set($key, $value)
+    {
+        //Check if we have a record with the given key
+        $record = static::findByName($key);
+        //If the key exists, update the value
+        if(is_object($record))
+        {
+            $record->value = $value;
+            $record->save();
+        }
+        //If the key does not exist, create a new instance
+        //and save it to the database
+        else
+        {
+            $record = static::create(['name' => $key, 'value' => $value]);
+        }
+        //Return the object
+        return $record;
     }
 }
