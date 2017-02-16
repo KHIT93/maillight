a8739ee033ae91d55e86175d34d9274c8f4ab903
Kenneth Hansen <kenneth@vkdata.dk>
2017-02-16 15:13:13 +0100

    Add default settings to database. This might be modified when the codebase improves

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -12,11 +12,29 @@ class SettingsSeeder extends Seeder
      */
     public function run()
     {
-        Setting::create(['name' => 'QUARANTINE_DAYS_TO_KEEP', 'value' => '90']);
-        Setting::create(['name' => 'QUARANTINE_DAYS_TO_KEEP', 'value' => '90']);
-        Setting::create(['name' => 'BRANDING_NAME', 'value' => 'MailLight']);
-        Setting::create(['name' => 'BRANDING_LOGO', 'value' => '/images/ml_logo.png']);
-        Setting::create(['name' => 'BRANDING_NAME', 'value' => 'MailLight']);
+        Setting::create(['name' => 'app.log_days_to_keep', 'value' => '90']);
+
+        Setting::create(['name' => 'branding.name', 'value' => 'MailLight']);
+        Setting::create(['name' => 'branding.logo', 'value' => '/images/ml_logo.png']);
+        Setting::create(['name' => 'branding.support.email', 'value' => '']);
+        Setting::create(['name' => 'branding.support.email', 'value' => '']);
+        Setting::create(['name' => 'branding.support.show', 'value' => '0']);
+
+        Setting::create(['name' => 'mail.mailscanner.config_dir', 'value' => '']);
+        Setting::create(['name' => 'mail.mailscanner.lib_dir', 'value' => '']);
+        Setting::create(['name' => 'mail.mailscanner.executable', 'value' => '']);
+
+        Setting::create(['name' => 'mail.spamassassin.executable', 'value' => '']);
+        Setting::create(['name' => 'mail.spamassassin.rules_dir', 'value' => '']);
+        Setting::create(['name' => 'mail.spamassassin.preferences', 'value' => '']);
+
+        Setting::create(['name' => 'mail.log', 'value' => '']);
+
+        Setting::create(['name' => 'quarantine.days_to_keep', 'value' => config('app.log_days_to_keep')]);
+        Setting::create(['name' => 'quarantine.from_address', 'value' => 'postmaster@'.$_SERVER['SERVER_NAME']]);
+        Setting::create(['name' => 'quarantine.from_name', 'value' => config('branding.name')]);
+        Setting::create(['name' => 'quarantine.report_subject', 'value' => 'Message Quarantine Report']);
+        Setting::create(['name' => 'quarantine.report_days', 'value' => '7']);
         
     }
 }
