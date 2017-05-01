e8c7ad426cda0e57f5bf3a9e316c24f75b436c74
Kenneth Hansen <kenneth@khansen-it.dk>
2017-05-01 08:18:34 +0200

    Add Link to update SA Rules

--- resources/views/tools/index.blade.php
+++ resources/views/tools/index.blade.php
@@ -10,6 +10,7 @@
         <li><a href="/tools/mysql-status">MySQL Database status</a></li>
         <li><a href="/tools/mailscanner-config">View Mailscanner configuration</a></li>
         <li><a href="/tools/update-geoip" @click.prevent="update_geoip">Update GeoIP Database</a></li>
+        <li><a href="/sa/update-rules" @click.prevent="update_sa_rules">Update Spam Rule descriptions</a></li>
     </ul>
     <p class="menu-label">
         Links
