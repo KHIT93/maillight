8b6942cdc4e3986ccd2a4f333fab7caa0d6c1138
Kenneth Hansen <kenneth@khansen-it.dk>
2017-05-01 09:12:47 +0200

    Added names to all routes

--- routes/web.php
+++ routes/web.php
@@ -12,39 +12,39 @@
 */
 
 Route::group(['middleware' => ['auth']], function(){
-    Route::get('/', 'HomeController@index');
-    Route::get('/systeminfo', 'HomeController@systeminfo');
+    Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+    Route::get('/systeminfo', ['as' => 'systeminfo', 'uses' => 'HomeController@systeminfo']);
 
-    Route::get('/messages', 'MessagesController@index');
-    Route::get('/messages/{message}', 'MessagesController@show');
-    Route::get('/messages/{message}/content', 'MessagesController@read_mail');
-    Route::get('/lists', 'HomeController@lists');
-    Route::get('/quarantine', 'HomeController@quarantine');
+    Route::get('/messages', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+    Route::get('/messages/{message}', ['as' => 'message.detail', 'uses' => 'MessagesController@show']);
+    Route::get('/messages/{message}/content', ['as' => 'message.content', 'uses' => 'MessagesController@read_mail']);
+    Route::get('/lists', ['as' => 'lists', 'uses' => 'HomeController@lists']);
+    Route::get('/quarantine', ['as' => 'quarantine', 'uses' => 'HomeController@quarantine']);
 
-    Route::get('/reports', 'ReportsController@index');
-    Route::get('/reports/filter', 'ReportsController@get_filtered_data');
-    Route::get('/reports/filter/options', 'ReportsController@get_filter_options');
-    Route::get('/reports/filter/messages_by_date', 'ReportsController@filter_messages_by_date');
-    Route::post('/reports/filter', 'ReportsController@apply_filter');
-    Route::delete('/reports/filter/{filter}', 'ReportsController@remove_filter');
-    Route::get('/reports/messages', 'ReportsController@messages_report');
-    Route::get('/reports/messages-by-date', 'ReportsController@messages_by_date');
-    Route::get('/reports/audit', 'ReportsController@audit');
+    Route::get('/reports', ['as' => 'reports', 'uses' => 'ReportsController@index']);
+    Route::get('/reports/filter', ['as' => 'reports.filter.data', 'uses' => 'ReportsController@get_filtered_data']);
+    Route::get('/reports/filter/options', ['as' => 'reports.filter.options', 'uses' => 'ReportsController@get_filter_options']);
+    Route::get('/reports/filter/messages_by_date', ['as' => 'reports.filter.messages.by_date', 'uses' => 'ReportsController@filter_messages_by_date']);
+    Route::post('/reports/filter', ['as' => 'reports.filter.apply', 'uses' => 'ReportsController@apply_filter']);
+    Route::delete('/reports/filter/{filter}', ['as' => 'reports.filter.delete', 'uses' => 'ReportsController@remove_filter']);
+    Route::get('/reports/messages', ['as' => 'reports.messages', 'uses' => 'ReportsController@messages_report']);
+    Route::get('/reports/messages-by-date', ['as' => 'reports.message.by_date', 'uses' => 'ReportsController@messages_by_date']);
+    Route::get('/reports/audit', ['as' => 'reports.audit_log', 'uses' => 'ReportsController@audit']);
 
-    Route::get('/tools', 'HomeController@tools');
-    Route::get('/tools/mysql-status', 'ToolsController@mysql_status');
-    Route::get('/tools/mailscanner-config', 'ToolsController@mailscanner_config');
+    Route::get('/tools', ['as' => 'tools', 'uses' => 'HomeController@tools']);
+    Route::get('/tools/mysql-status', ['as' => 'tools.status.mysql', 'uses' => 'ToolsController@mysql_status']);
+    Route::get('/tools/mailscanner-config', ['as' => 'tools.status.mailscanner', 'uses' => 'ToolsController@mailscanner_config']);
 
-    Route::get('/user', 'UsersController@show');
-    Route::get('/users', 'UsersController@index');
-    Route::get('/users/create', 'UsersController@create');
-    Route::put('/users/create', 'UsersController@store');
-    Route::get('/users/{user}/update', 'UsersController@edit');
-    Route::patch('/users/{user}/update', 'UsersController@update');
-    Route::get('/users/{user}/delete', 'UsersController@delete');
-    Route::delete('/users/{user}/delete', 'UsersController@destroy');
+    Route::get('/user', ['as' => 'users.show', 'uses' => 'UsersController@show']);
+    Route::get('/users', ['as' => 'users', 'uses' => 'UsersController@index']);
+    Route::get('/users/create', ['as' => 'users.create', 'uses' => 'UsersController@create']);
+    Route::put('/users/create', ['as' => 'users.store', 'uses' => 'UsersController@store']);
+    Route::get('/users/{user}/update', ['as' => 'users.edit', 'uses' => 'UsersController@edit']);
+    Route::patch('/users/{user}/update', ['as' => 'users.update', 'uses' => 'UsersController@update']);
+    Route::get('/users/{user}/delete', ['as' => 'users.delete', 'uses' => 'UsersController@delete']);
+    Route::delete('/users/{user}/delete', ['as' => 'users.destroy', 'uses' => 'UsersController@destroy']);
 
-    Route::get('/settings', 'SettingsController@index');
+    Route::get('/settings', ['as' => 'settings', 'uses' => 'SettingsController@index']);
 });
 
 Auth::routes();
